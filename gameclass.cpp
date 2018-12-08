32ad958bb265d10186647a3550532d1c73ff896b
Archie7777 <1751184819@qq.com>
2018-12-08 19:52:57 +0800

    优化了生成数独函数

--- gameclass.cpp
+++ gameclass.cpp
@@ -1,3 +1,8 @@
+//*******************************************
+//game类
+//说明： 完成数独输入以及解数独功能
+//*******************************************
+
 #include "pch.h"
 #include <cstdio>
 #include <iostream>
@@ -6,7 +11,6 @@
 #include <ctime>
 #include <fstream>
 #include <algorithm>
-#include <stack>
 #include <vector>
 using namespace std;
 #include "sourse.h"
@@ -21,14 +25,16 @@ game::game(char* path)
 	}
 	else {
 		puzzle_path = (char*)malloc(sizeof(char) * (strlen(path) + 1));
-		if (puzzle_path != NULL)
+		if (puzzle_path != NULL) {
 			strcpy_s(puzzle_path, strlen(path) + 1, path);
+		}
 	}
-	if (file != NULL)
+	if (file != NULL) {
 		fclose(file);
+	}
 }
 
-void game::read()
+void game::solution()
 {
 	errno_t err;
 	FILE* readfile;
@@ -39,20 +45,26 @@ void game::read()
 	if (readfile == 0) return;
 	for (int count = 0; ; count++)
 	{
+		// read puzzle
 		char temp;
+		int number_temp;
 		for (int i = 0; i < 9; i++) {
 			for (int j = 0; j < 9; j++) {
-				int end = fscanf_s(readfile, "%d%c", &sudoku[i][j], &temp, (unsigned int)(sizeof(int) + sizeof(char)));
+				int end = fscanf_s(readfile, "%d%c", &number_temp, &temp, (unsigned int)(sizeof(int) + sizeof(char)));
+				if (number_temp == 0) vec.push_back(pair<int, int>(i, j));
+				else sudoku[i][j] = 1 << (number_temp - 1);
 				if (end == EOF) flagtobreak = 1;
 			}
 		}
 		if (flagtobreak == 1) break;
-		solve_puzzle();
+		//solve
+		dfs(0);
+		//write puzzle
 		if (count != 0) fprintf(writefile, "\n\n");
 		for (int i = 0; i < 9; i++) {
 			for (int j = 0; j < 9; j++) {
-				if (i == 8 && j == 8) fprintf(writefile, "%d", sudoku[i][j]);
-				else fprintf(writefile, "%d%c", sudoku[i][j], j == 8 ? '\n' : ' ');
+				if (i == 8 && j == 8) fprintf(writefile, "%d", get_index(sudoku[i][j]));
+				else fprintf(writefile, "%d%c", get_index(sudoku[i][j]), j == 8 ? '\n' : ' ');
 			}
 		}
 	}
@@ -60,115 +72,31 @@ void game::read()
 	return;
 }
 
-void game::solve_puzzle()
+int game::get_index(int num)
 {
-	memset(confirm, 0, sizeof(confirm));
-	memset(possible_number, -1, sizeof(possible_number));
-	for (int i = 0; i < 9; i++) {//更新所有的状态值
-		for (int j = 0; j < 9; j++)
-		{
-			if (sudoku[i][j] != 0) {
-				fillnum(i, j, sudoku[i][j]);
-			}
-		}
+	int ans = 0;
+	while (num) {
+		ans++;
+		num >>= 1;
 	}
-	while (1)//找唯一的空
-	{
-		int flag_of_break = 1;
-		int count = 0;
-		for (int i = 0; i < 9; i++)
-			for (int j = 0; j < 9; j++) {
-				if (confirm[i][j] == 1) continue;
-				count = 0;
-				int temp = 0;
-				for (int x = 1; x <= 9; x++)
-				{
-					
-					if (possible_number[i][j][x] != 0) {
-						count++;
-						temp = possible_number[i][j][x];
-					}
-				}
-				if (count == 1) sudoku[i][j] = temp;
-			}
-		if (flag_of_break == 1) break;
-	}
-	//没有唯一的空了，开始dfs
-	stack<int> s;
-	int find_first = 0;
+	return ans;
+}
+
+int game::dfs(int now)
+{
+	int row = vec[now].first;
+	int line = vec[now].second;
+	int f = (1 << 9) - 1;
 	for (int i = 0; i < 9; i++) {
-		for (int j = 0; j < 9; j++)
-		{
-			if (confirm[i][j] == 0) {
-				s.push(i * 9 + j);
-				for (int x = 1; x <= 9; x++) {
-					if (possible_number[i][j][x] != 0) {
-						sudoku[i][j] = x;
-						find_first = 1;
-						break;
-					}
-				}
-				if (find_first == 1) break;
-			}
-		}
-		if (find_first == 1) break;
+		f &= (~sudoku[row][i]) & ((~sudoku[i][line]) & (~sudoku[row / 3 * 3 + i / 3][line / 3 * 3 + i % 3]));
 	}
-	while (1)
+	while (f)
 	{
-		int there_is_right_number = 0;
-		int flag_to_break = 0;
-		int find_empty = 0;
-		for (int i = s.empty() ? 0 : (s.top() / 9); i < 9; i++) {
-			for (int j = s.empty() ? 0 : (i == s.top() / 9 ? s.top() % 9 + 1 : 0); j < 9; j++) {
-				if (confirm[i][j] == 0) {
-					find_empty = 1;
-					for (int x = sudoku[i][j] + 1; x <= 9; x++)
-					{
-						if (x == 0) continue;
-						if (possible_number[i][j][x] != 0 && jdg_rep(i, j, x, sudoku) == 1) {
-							there_is_right_number = 1;
-							s.push(i * 9 + j);
-							sudoku[i][j] = x;
-							flag_to_break = 1;
-							break;
-						}
-					}
-					if (there_is_right_number == 0) {
-						s.pop();
-						sudoku[i][j] = 0;
-						flag_to_break = 1;
-						break;
-					}
-				}
-				if (flag_to_break == 1) break;
-			}
-			if (flag_to_break == 1) break;
-		}
-		if (find_empty == 0) break;
+		sudoku[row][line] = f & (-f);
+		f &= ~sudoku[row][line];
+		if (now < vec.size() - 1 && !dfs(now + 1)) continue;
+		else return true;
 	}
-	return;
+	sudoku[row][line] = 0;
+	return false;
 }
-
-void game::fillnum(int row, int line, int putnum)
-{
-	confirm[row][line] = 1;
-	int srow_of_matrix = row - row % 3;
-	int sline_of_matrix = line - line % 3;
-	for (int i = 0; i < 9; i++)
-	{
-		possible_number[row][i][putnum] = 0;
-		possible_number[i][line][putnum] = 0;
-		possible_number[srow_of_matrix + i / 3][sline_of_matrix + i % 3][putnum] = 0;
-	}
-	sudoku[row][line] = putnum;
-}
-
-//void game::output(FILE* file)
-//{
-//	for (int i = 0; i < 9; i++) {
-//		for (int j = 0; j < 9; j++){
-//			fprintf(file, "%d%c", sudoku[i][j], j == 8 ? '\n' : ' ');
-//		}
-//	}
-//	fprintf(file, "\n");
-//}
